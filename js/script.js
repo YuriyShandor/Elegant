456ba574b1c819c03ace53f2c4fc2747044740af
Yuriy Shandor <yuriyshandor@gmail.com>
2017-11-10 16:32:02 +0200

    Add Section Our_Works. Mod_02

--- js/script.js
+++ js/script.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
   $('.toggler-icon').on('click', function(){
     $('.nav-bar').slideToggle('0.5s');
   });
+
+  var products = ['all', 'brending', 'web-design', 'mobile-ui', 'illustration'];
+
+  $('.work-btn__all').addClass('work-btn_active');
+
+  products.forEach(function(item) {
+    $(`.work-btn__${item}`).click(function(){
+      $('.our-works__class').removeClass('work-btn_active');
+      $(this).addClass('work-btn_active');
+      $('.work__all').addClass('work-hidden');
+      $(`.work__${item}`).removeClass('work-hidden');
+    });
+  });
 });
